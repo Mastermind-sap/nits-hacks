0951385cc25c1f80a3d93f625489a37872b360da
krishnam <maheshwarikrishnam123@gmail.com>
2024-02-04 07:38:52 +0530

    UPD: game1

--- minigames/dodge_the_emotions/HUD.gd
+++ minigames/dodge_the_emotions/HUD.gd
@@ -11,7 +11,7 @@ func show_message(text):
 func show_game_over():
 	show_message("Game Over")
 	await $MessageTimer.timeout
-	$MessageLabel.text = "Dodge the\nEmotions"
+	$MessageLabel.text = "Dodge the\nNegative Emotions"
 	$MessageLabel.show()
 	await get_tree().create_timer(1).timeout
 	$StartButton.show()
